0590d96322f9f220fb3efd53d67235a7f5c8fc48
aattoa <126791757+aattoa@users.noreply.github.com>
2023-08-23 14:44:20 +0300

    Add DeBruijn.toSyntax

--- src/DeBruijn.hs
+++ src/DeBruijn.hs
@@ -3,6 +3,7 @@ module DeBruijn
 , Expression(..)
 , showExpression
 , fromSyntax
+, toSyntax
 ) where
 
 import Data.List (elemIndex)
@@ -34,13 +35,34 @@ showExpression = \case
         name
 
 
-fromSyntax :: [String] -> Syntax.Expression -> Expression
-fromSyntax variables = \case
-    Syntax.Application invocable argument ->
-        Application (fromSyntax variables invocable) (fromSyntax variables argument)
-    Syntax.Abstraction parameter body ->
-        Abstraction (fromSyntax (parameter : variables) body)
-    Syntax.Variable name ->
-        case elemIndex name variables of
-            Just index -> BoundVariable (Index index)
-            Nothing    -> UnboundVariable name
+makeParameterName :: Int -> String
+makeParameterName n = (names !! mod n count) : replicate (div n count) '\''
+    where names = ['a'..'z']
+          count = length names
+
+
+fromSyntax :: Syntax.Expression -> Expression
+fromSyntax = impl [] where
+    impl variables = \case
+        Syntax.Application invocable argument ->
+            Application (impl variables invocable) (impl variables argument)
+        Syntax.Abstraction parameter body ->
+            Abstraction (impl (parameter : variables) body)
+        Syntax.Variable name ->
+            case elemIndex name variables of
+                Just index -> BoundVariable (Index index)
+                Nothing    -> UnboundVariable name
+
+
+toSyntax :: Expression -> Syntax.Expression
+toSyntax = impl [] where
+    impl parameters = \case
+        Application invocable argument ->
+            Syntax.Application (impl parameters invocable) (impl parameters argument)
+        Abstraction body ->
+            let parameter = makeParameterName (length parameters)
+            in Syntax.Abstraction parameter (impl (parameter : parameters) body)
+        BoundVariable (Index index) ->
+            Syntax.Variable (parameters !! index)
+        UnboundVariable name ->
+            Syntax.Variable name
